34680f3949faa3674ffc49f2ada21b0e06024a32
aniket2191 <106371397+aniket2191@users.noreply.github.com>
2022-05-31 22:15:46 +0530

    Update TestBase.java

--- RestAPITesting_EmployeeProject/src/test/java/com/employeeapi/Base/TestBase.java
+++ RestAPITesting_EmployeeProject/src/test/java/com/employeeapi/Base/TestBase.java
@@ -14,7 +14,7 @@ public class TestBase {
 	public RequestSpecification httpRequest;
 	public Response response;
 	public Logger logger;
-	public int empId=2329; //hardcoded value
+	public int empId=13983; //hardcoded value
 
   @BeforeClass
   public void setUp() {
